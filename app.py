a7d3163980859024e0182533f227ef6d1f884a7c
Ashtray Sandbag <37274137+akshanbhag@users.noreply.github.com>
2023-10-31 19:50:10 -0400

    changed modify games functionality

--- app.py
+++ app.py
@@ -16,51 +16,75 @@ def display_data():
 
     return render_template('index.html', data=data)
 
-@app.route('/add', methods=['POST'])
+def display_games():
+    conn = sqlite3.connect('your_database.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM Games')
+    games = cursor.fetchall()
+
+    conn.close()
+
+    return render_template('display_games.html', games=games, teams=get_teams())
+
+@app.route('/add_game', methods=['POST'])
 def add_game():
     if request.method == 'POST':
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        name = request.form['name']
-        # Include additional fields for your "Games" table
-        # For example, you may have fields like date, location, score, etc.
+        home_team_id = request.form['home_team']
+        away_team_id = request.form['away_team']
+        winner_id = request.form['winner']
+        date = request.form['date']
+        week_number = request.form['week_number']
 
-        cursor.execute('INSERT INTO Games (name) VALUES (?)', (name,))
+        conn = sqlite3.connect('your_database.db')
+        cursor = conn.cursor()
+
+        cursor.execute('INSERT INTO Games (home_team_id, away_team_id, winner_id, date, week_number) VALUES (?, ?, ?, ?, ?)',
+                       (home_team_id, away_team_id, winner_id, date, week_number))
+
         conn.commit()
         conn.close()
 
     return redirect(url_for('display_games'))
 
-@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+@app.route('/edit_game/<int:id>', methods=['GET', 'POST'])
 def edit_game(id):
-    conn = get_db_connection()
+    conn = sqlite3.connect('your_database.db')
     cursor = conn.cursor()
 
     if request.method == 'POST':
-        name = request.form['name']
-        # Include additional fields for your "Games" table
-        # For example, you may have fields like date, location, score, etc.
+        home_team_id = request.form['home_team']
+        away_team_id = request.form['away_team']
+        winner_id = request.form['winner']
+        date = request.form['date']
+        week_number = request.form['week_number']
 
-        cursor.execute('UPDATE Games SET name = ? WHERE id = ?', (name, id))
+        cursor.execute('UPDATE Games SET home_team_id=?, away_team_id=?, winner_id=?, date=?, week_number=? WHERE id=?',
+                       (home_team_id, away_team_id, winner_id, date, week_number, id))
+
         conn.commit()
         conn.close()
+
         return redirect(url_for('display_games'))
 
     cursor.execute('SELECT * FROM Games WHERE id = ?', (id,))
     game = cursor.fetchone()
+
     conn.close()
 
-    return render_template('edit_game.html', game=game)
+    return render_template('edit_game.html', game=game, teams=get_teams())
 
-@app.route('/delete/<int:id>')
+@app.route('/delete_game/<int:id>')
 def delete_game(id):
-    conn = get_db_connection()
+    conn = sqlite3.connect('your_database.db')
     cursor = conn.cursor()
 
     cursor.execute('DELETE FROM Games WHERE id = ?', (id,))
+
     conn.commit()
     conn.close()
 
     return redirect(url_for('display_games'))
+
 if __name__ == '__main__':
     app.run(debug=True)
