57a60a62ca9c20544c1691d870ce31712628a956
Ashtray Sandbag <37274137+akshanbhag@users.noreply.github.com>
2023-11-02 01:31:11 -0400

    added report functionality

--- seasontracker/nflseason/views.py
+++ seasontracker/nflseason/views.py
@@ -49,18 +49,22 @@ def delete_game(request, game_id):
     game.delete()
     return HttpResponseRedirect(reverse('games_interface'))
 
-def report_interface(request):
-    years = list(range(1920, 2024))  # Years from 1920 to 2023
-    weeks = list(range(1, 24))       # Weeks from 1 to 23
-    
+def report_interface(request):    
     if request.method == 'POST':
-        # Retrieve the selected filters (year and week) from the form
-        selected_year = request.POST.get('year')
-        selected_week = request.POST.get('week')
+        year = request.POST.get('year')
+        week_number = request.POST.get('week_number')
         
-        # Query the Games table based on the selected filters
-        filtered_games = Games.objects.filter(year=selected_year, week_number=selected_week)
-        
-        return render(request, 'report_interface.html', {'years': years, 'weeks': weeks, 'filtered_games': filtered_games})
-    
-    return render(request, 'report_interface.html', {'years': years, 'weeks': weeks, 'filtered_games': None})
+        if year:
+            # Filter games by year
+            games = Games.objects.filter(date_played__year=year)
+            
+            if week_number:
+                # Filter further by week number
+                games = games.filter(week_number=week_number)
+        else:
+            games = Games.objects.all()
+
+        return render(request, 'report_interface.html', {'games': games})
+    else:
+        games = Games.objects.all()
+        return render(request, 'report_interface.html', {'games': games})
