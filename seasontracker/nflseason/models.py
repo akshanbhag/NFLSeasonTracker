6b5a957b27328b66f73ba5ca3c10cf1882326266
Ashtray Sandbag <37274137+akshanbhag@users.noreply.github.com>
2023-11-02 12:18:57 -0400

    added comments to models.py

--- seasontracker/nflseason/models.py
+++ seasontracker/nflseason/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+#Games table
 class Games(models.Model):
     game_id = models.AutoField(primary_key=True)
     home_team = models.IntegerField()
@@ -8,11 +9,13 @@ class Games(models.Model):
     date_played = models.DateField()
     week_number = models.IntegerField()
 
+#Teams table
 class Teams(models.Model):
     team_id = models.AutoField(primary_key=True)
     division = models.CharField(max_length=50)
     team_name = models.CharField(max_length=100)
 
+#Players table
 class Players(models.Model):
     player_id = models.AutoField(primary_key=True)
     position = models.CharField(max_length=50)
@@ -20,12 +23,14 @@ class Players(models.Model):
     last_name = models.CharField(max_length=50)
     team_id = models.IntegerField()
 
+#Team_Record table
 class Team_Record(models.Model):
     team_id = models.IntegerField()
     wins = models.IntegerField()
     year = models.IntegerField()
     week_number = models.IntegerField()
 
+#Season table
 class Season(models.Model):
     year = models.IntegerField(primary_key=True)
     superbowl_champ = models.CharField(max_length=100)
